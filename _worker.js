aae31e8c402e80a8386e9df57f8853067cb1403c
lingdayyds <lingdayyds@gmail.com>
2024-07-20 19:53:02 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'ac039bbc-5b03-4109-ac7f-818e6f1948d6';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
